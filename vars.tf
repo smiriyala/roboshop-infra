121040f1c44f5715483e257b64df49c71db38a54
subbum <subbu@gmail.com>
2023-05-24 12:34:50 +0100

    only frontend enabled

--- vars.tf
+++ vars.tf
@@ -4,45 +4,45 @@ variable "instances" {
         name = "frontend"
         type = "t3.micro"
     }
-    mongo = {
-        name = "mongo"
-        type = "t3.micro"
-    }
-    catalogue = {
-        name = "catalogue"
-        type = "t3.micro"
-    }
-    redis = {
-        name = "redis"
-        type = "t3.micro"
-    }
-    user = {
-        name = "user"
-        type = "t3.micro"
-    }
-    cart = {
-        name = "cart"
-        type = "t3.micro"
-    }
-    mysql = {
-        name = "mysql"
-        type = "t3.micro"
-    }
-    shipping = {
-        name = "shipping"
-        type = "t3.micro"
-    }
-    rabbitmq = {
-        name = "rabbitmq"
-        type = "t3.micro"
-    }
-    payment = {
-        name = "payment"
-        type = "t3.micro"
-    }
-    dispatch = {
-        name = "dispatch"
-        type = "t3.micro"
-    }
+    # mongo = {
+    #     name = "mongo"
+    #     type = "t3.micro"
+    # }
+    # catalogue = {
+    #     name = "catalogue"
+    #     type = "t3.micro"
+    # }
+    # redis = {
+    #     name = "redis"
+    #     type = "t3.micro"
+    # }
+    # user = {
+    #     name = "user"
+    #     type = "t3.micro"
+    # }
+    # cart = {
+    #     name = "cart"
+    #     type = "t3.micro"
+    # }
+    # mysql = {
+    #     name = "mysql"
+    #     type = "t3.micro"
+    # }
+    # shipping = {
+    #     name = "shipping"
+    #     type = "t3.micro"
+    # }
+    # rabbitmq = {
+    #     name = "rabbitmq"
+    #     type = "t3.micro"
+    # }
+    # payment = {
+    #     name = "payment"
+    #     type = "t3.micro"
+    # }
+    # dispatch = {
+    #     name = "dispatch"
+    #     type = "t3.micro"
+    # }
   }
 }
