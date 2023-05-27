b59bcd21f935c5d6229064ec9c78dd82fde0ac34
subbum <subbu@gmail.com>
2023-05-28 00:25:27 +0100

    removed IAM policy mapping to ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "ec2" {
     ami = data.aws_ami.ami.image_id
     instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.sg.id]
-    iam_instance_profile = aws_iam_instance_profile.profile.name
+    #iam_instance_profile = aws_iam_instance_profile.profile.name
     tags = {
         Name = var.component
     }
