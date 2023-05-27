2ce23cb4be41fa22678c451df3a67217f939d2fc
subbum <subbu@gmail.com>
2023-05-27 17:06:08 +0100

    updated space in policy creation

--- ec2/main.tf
+++ ec2/main.tf
@@ -88,7 +88,7 @@ resource "aws_iam_policy" "ssm-policy" {
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
-   "Version": "2012-10-17",
+    "Version": "2012-10-17",
     "Statement": [
       {
         "Sid": "VisualEditor0",
