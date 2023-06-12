9c8e5ab3eab5995550840aa479a23f9ec54ce32c
subbum <subbu@gmail.com>
2023-06-12 09:47:39 +0100

    updated soruce

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 module "vpc" {
   env = var.env
-  source = "https://github.com/smiriyala/tf-module-vpc.git"
-
+  source = "git::https://github.com/smiriyala/tf-module-vpc.git"
   for_each = var.vpc
   vpc_cidr = each.value["vpc_cidr"]
-
-
 }
