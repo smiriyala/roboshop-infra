05142d00788680da1fff97b160b50156a3448199
subbum <subbu@gmail.com>
2023-06-14 18:56:52 +0100

    updated subnets

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "vpc" {
   env = var.env
   source = "git::https://github.com/smiriyala/tf-module-vpc.git"
   for_each = var.vpc
-  vpc_cidr = each.value["vpc_cidr"]
+  vpc_cidr = each.value[ "vpc_cidr" ]
   tags = var.tags
-  public_subnets = each.value["public_subnets"]
+  public_subnets = each.value[ "public_subnets" ]
   private_subnets = each.value[ "private_subnets"]
 }
 
