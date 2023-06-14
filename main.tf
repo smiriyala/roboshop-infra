0c9faa017c052a8ca31a2d46a8213ef236a0aaee
subbum <subbu@gmail.com>
2023-06-14 14:58:42 +0100

    added vpc output to recognize vpc data loading

--- main.tf
+++ main.tf
@@ -7,3 +7,11 @@ module "vpc" {
   public_subnets = each.value["public_subnets"]
   private_subnets = each.value[ "private_subnets"]
 }
+
+
+##just help to see how the output would be...data
+# NOTE: It doenst return anything as it need be configured OUTPUT file in resource module.
+output "vpcData" {
+  value = module.vpc.pu
+  
+}
