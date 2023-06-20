2896d69df850d273ec747a6102e1090d0bbb5c2d
subbum <subbu@gmail.com>
2023-06-20 13:56:59 +0100

    remove provider

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 resource "aws_ssm_parameter" "parameters" {
     count = length(var.parameters)
     name = var.parameters[count.index].name
